bf6fb59282bb02e7445b32d36a001068383e6403
DIA4A <71932607+DIA4A@users.noreply.github.com>
2023-10-27 05:15:07 +0300

    Add files via upload

---
+++ Dumper Module/DllMain.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <windows.h>
+#include <psapi.h>
+
+#include "MinHook.h"
+
+
+#define INRANGE(x,a,b)    (x >= a && x <= b) 
+#define GETBITS( x )    (INRANGE((x&(~0x20)),'A','F') ? ((x&(~0x20)) - 'A' + 0xa) : (INRANGE(x,'0','9') ? x - '0' : 0))
+#define GETBYTE( x )    (GETBITS(x[0]) << 4 | GETBITS(x[1]))
+
+DWORD64 FindPattern(DWORD64 m_dwAddress, const std::string& m_strPattern)
+{
+	const char* m_szPattern = m_strPattern.c_str();
+	DWORD64 m_dwFirstMatch = 0;
+
+	MODULEINFO m_pModuleInformation;
+	K32GetModuleInformation(GetCurrentProcess(), (HMODULE)m_dwAddress, &m_pModuleInformation, sizeof(MODULEINFO));
+	DWORD64 m_dwEndAddress = m_dwAddress + m_pModuleInformation.SizeOfImage;
+
+	for (DWORD64 m_dwCurrentAddress = m_dwAddress; m_dwCurrentAddress < m_dwEndAddress; m_dwCurrentAddress++)
+	{
+		if (!*m_szPattern)
+		{
+			return m_dwFirstMatch;
+		}
+
+		if (*(PBYTE)m_szPattern == '\?' || *(BYTE*)m_dwCurrentAddress == GETBYTE(m_szPattern))
+		{
+			if (!m_dwFirstMatch)
+			{
+				m_dwFirstMatch = m_dwCurrentAddress;
+			}
+
+			if (!m_szPattern[2])
+			{
+				return m_dwFirstMatch;
+			}
+
+			(*(PWORD)m_szPattern == '\?\?' || *(PBYTE)m_szPattern != '\?') ? m_szPattern += 3 : m_szPattern += 2;
+		}
+		else
+		{
+			m_szPattern = m_strPattern.c_str();
+			m_dwFirstMatch = 0;
+		}
+	}
+
+	return 0;
+}
+
+void* DecryptMetadataHeaderOriginal = nullptr;
+void* __stdcall DecryptMetadataHeader(void* a1, DWORD64 m_dwHeaderSize, void* a3, void* a4)
+{
+    void* m_pResult = ((decltype(&DecryptMetadataHeader))DecryptMetadataHeaderOriginal)(a1, m_dwHeaderSize, a3, a4);
+
+	FILE* m_pDecryptedMetadataHeaderFile = nullptr;
+	fopen_s(&m_pDecryptedMetadataHeaderFile, "DIA4A\\decrypted-metadata-header.dat", "wb");
+	fwrite(m_pResult, 1, m_dwHeaderSize, m_pDecryptedMetadataHeaderFile);
+	fclose(m_pDecryptedMetadataHeaderFile);
+
+	return m_pResult;
+}
+
+BOOL APIENTRY DllMain(HMODULE m_pModule, DWORD m_dwReason, LPVOID m_pReserved)
+{
+	if (m_dwReason == DLL_PROCESS_ATTACH)
+	{
+		DWORD64 m_dwGameAssemblyAddress = (DWORD64)LoadLibraryA("GameAssembly.dll");
+
+		DWORD64 m_dwXXTEADecrypt = FindPattern(m_dwGameAssemblyAddress, "4C 89 4C 24 20 53 56 41 55 41 56 48 83 EC 58 41");
+		if (m_dwXXTEADecrypt == 0)
+		{
+			MessageBoxA(NULL, "Failed To Find The xxtea Decrypt Function", "Dumper Module", MB_OK);
+			return TRUE;
+		}
+
+		MH_Initialize();
+
+		MH_CreateHook((void*)m_dwXXTEADecrypt, DecryptMetadataHeader, &DecryptMetadataHeaderOriginal);
+
+		MH_EnableHook(MH_ALL_HOOKS);
+	}
+    return TRUE;
+}
+
---
+++ Exalt Metadata Dumper/Main.cpp
@@ -0,0 +1,262 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <map>
+#include <windows.h>
+
+std::vector<unsigned char> LoadFileBuffer(const std::string& m_strFile)
+{
+	std::vector<unsigned char> m_pBuffer;
+	std::ifstream m_pFile(m_strFile.c_str(), std::ios::in | std::ios::binary | std::ios::ate);
+
+	std::streamsize m_nSize = 0;
+	if (m_pFile.seekg(0, std::ios::end).good())
+	{
+		m_nSize = m_pFile.tellg();
+	}
+
+	if (m_pFile.seekg(0, std::ios::beg).good())
+	{
+		m_nSize -= m_pFile.tellg();
+	}
+
+	if (m_nSize > 0)
+	{
+		m_pBuffer.resize((std::size_t)m_nSize);
+		m_pFile.read((char*)(&m_pBuffer[0]), m_nSize);
+	}
+
+	return m_pBuffer;
+}
+
+int main()
+{
+	CreateDirectoryA("DIA4A", NULL);
+
+	PROCESS_INFORMATION m_pProcessInformation;
+	memset(&m_pProcessInformation, 0, sizeof(m_pProcessInformation));
+
+	STARTUPINFOA m_pStartupInfo;
+	memset(&m_pStartupInfo, 0, sizeof(m_pStartupInfo));
+
+	bool m_bProcessCreated = CreateProcessA(
+		"RotMG Exalt.exe", NULL,
+		NULL, NULL, 
+		NULL, CREATE_SUSPENDED,
+		NULL, NULL, 
+		&m_pStartupInfo, &m_pProcessInformation
+	);
+
+	if (!m_bProcessCreated)
+	{
+		printf("[-] Failed To Start Exalt");
+		getchar();
+		return 1;
+	}
+
+	printf("[+] Started Exalt Successfully!\n");
+
+	HANDLE m_pProcessHandle = m_pProcessInformation.hProcess;
+
+	std::string m_strDumperModuleName = "DumperModule.dll";
+
+	void* m_pRemoteDumperModuleName = VirtualAllocEx(m_pProcessHandle, NULL, m_strDumperModuleName.size() + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	DWORD m_dwWPMResult = WriteProcessMemory(m_pProcessHandle, m_pRemoteDumperModuleName, m_strDumperModuleName.data(), m_strDumperModuleName.size(), NULL);
+
+	if (m_pRemoteDumperModuleName == nullptr || m_dwWPMResult == 0)
+	{
+		printf("[-] Failed To Write Module String In Exalt\n");
+		getchar();
+		return 1;
+	}
+
+	printf("[+] Wrote Module String In Exalt Successfully!\n");
+
+	void* m_pLoadLibraryA = GetProcAddress(GetModuleHandleA("kernelbase.dll"), "LoadLibraryA");
+	if (m_pLoadLibraryA == nullptr)
+	{
+		printf("[-] Failed To Find LoadLibraryA\n");
+		getchar();
+		return 1;
+	}
+
+	HANDLE m_pRemoteThread = CreateRemoteThread(m_pProcessHandle, (LPSECURITY_ATTRIBUTES)NULL, NULL, (LPTHREAD_START_ROUTINE)m_pLoadLibraryA, m_pRemoteDumperModuleName, NULL, NULL);
+	if (m_pRemoteThread == nullptr)
+	{
+		printf("[-] Failed To Create Module Mapping Thread\n");
+		getchar();
+		return 1;
+	}
+
+	printf("[+] Mapped Dumper Module Into Memory Successfully!\n"); // bs but cba to find a better way to express the above
+
+	if (WaitForSingleObject(m_pRemoteThread, 10000) != WAIT_OBJECT_0)
+	{
+		printf("[-] Something Failed While Waiting For The Module Dumper\n");
+		getchar();
+		return 1;
+	}
+
+	printf("[+] The Module Dumper Has Been Loaded!\n");
+	printf("[+] Waiting For A Maximum Of 30 Seconds For The Metadata Header To Be Dumped\n");
+
+	// delete it incase it already exists so we can check for a new one
+	_unlink("DIA4A\\decrypted-metadata-header.dat");
+
+	ResumeThread(m_pProcessInformation.hThread);
+
+	bool m_bDumpFinished = false;
+	for (int i = 0; i < 300; i++)
+	{
+		Sleep(100);
+
+		if (std::ifstream("DIA4A\\decrypted-metadata-header.dat").is_open())
+		{
+			m_bDumpFinished = true;
+			break;
+		}
+	}
+
+	TerminateProcess(m_pProcessInformation.hProcess, 0);
+
+	if (!m_bDumpFinished)
+	{
+		printf("[-] Failed To Dump Metadata Header After 30 Seconds Of Waiting\n");
+		getchar();
+		return 1;
+	}
+
+	printf("[+] The Decrypted Metadata Header Should Now Be Dumped In \"DIA4A\\decrypted-metadata-header.dat\"\n");
+
+	printf("[+] Trying To Unshuffle The Header\n");
+	{
+		std::vector<unsigned char> m_pDecryptedMetadataBuffer = LoadFileBuffer("DIA4A\\decrypted-metadata-header.dat");
+
+		int m_nIntegerCount = m_pDecryptedMetadataBuffer.size() / sizeof(int);
+		std::vector<int> m_nDecryptedMetadata(m_nIntegerCount);
+		memcpy(m_nDecryptedMetadata.data(), m_pDecryptedMetadataBuffer.data(), m_pDecryptedMetadataBuffer.size());
+
+		// the real metadata header consists of 31 offsets, sizes, and then the version and sanity
+		// since we already know the highest offset appears 3 times, we can just work backwards off it
+
+		int m_nHighestRealOffset = 0;
+		std::map<int, int> m_nOffsetInstances;
+		for (int i = 0; i < m_nIntegerCount; i++)
+		{
+			int& m_nCurrentOffsetInstances = m_nOffsetInstances[m_nDecryptedMetadata[i]];
+			m_nCurrentOffsetInstances++;
+
+			if (m_nCurrentOffsetInstances == 3 && m_nDecryptedMetadata[i] > 1000000)
+			{
+				m_nHighestRealOffset = m_nDecryptedMetadata[i];
+				break;
+			}
+		}
+
+		int m_nGuessedSize = 0;
+		for (int i = 0; i < m_nIntegerCount; i++)
+		{
+			if (m_nDecryptedMetadata[i] < 7000 || m_nDecryptedMetadata[i] > 11000 || m_nDecryptedMetadata[i] % 4 != 0)
+			{
+				continue;
+			}
+
+			m_nGuessedSize = m_nDecryptedMetadata[i];
+			break;
+		}
+
+		std::vector<std::pair<int, int>> m_nPairs;
+		// since its unclear what the size of the highest offset is, we'll guess within a range
+		m_nPairs.push_back({ m_nHighestRealOffset, m_nGuessedSize });
+		m_nPairs.push_back({ m_nHighestRealOffset, 0 });
+		m_nPairs.push_back({ m_nHighestRealOffset, 0 });
+
+
+		int m_nOffsetsLeft = 28;
+		int m_nCurrentOffset = m_nHighestRealOffset;
+
+		for (int m_nRunInstance = 0; m_nRunInstance < 28; m_nRunInstance++)
+		{
+			for (int i = 0; i < m_nIntegerCount; i++)
+			{
+				if (m_nDecryptedMetadata[i] <= 0 || m_nDecryptedMetadata[i] % 4 != 0)
+				{
+					continue;
+				}
+
+				bool m_bFound = false;
+				for (int j = 0; j < m_nIntegerCount; j++)
+				{
+					if (m_nDecryptedMetadata[j] <= 0)
+					{
+						continue;
+					}
+
+					// m_nCurrentOffset = current offset
+					// m_nDecryptedMetadata[i] = size of previous offset
+					// m_nDecryptedMetadata[j] = previous offset
+
+					int m_nOffsetDelta = m_nCurrentOffset - m_nDecryptedMetadata[j];
+					if (std::abs(m_nDecryptedMetadata[i] - m_nOffsetDelta) <= 20)
+					{
+						m_nPairs.push_back({ m_nDecryptedMetadata[j], m_nDecryptedMetadata[i] });
+						m_nCurrentOffset = m_nDecryptedMetadata[j];
+						m_nOffsetsLeft--;
+
+						m_nDecryptedMetadata[i] = 0;
+						m_nDecryptedMetadata[j] = 0;
+
+						m_bFound = true;
+						break;
+					}
+				}
+
+				if (m_bFound)
+				{
+					break;
+				}
+			}
+		}
+
+		m_nPairs.push_back({ 0xFAB11BAF, 29 }); // add the sanity and version
+		std::reverse(m_nPairs.begin(), m_nPairs.end());
+
+		if (m_nOffsetsLeft == 0)
+		{
+			printf("[+] Unshuffled All Offsets Successfully, Saving As \"DIA4A\\unshuffled-metadata-header.dat\"\n");
+
+			int m_nUnshuffledHeader[64];
+			for (int i = 0; i < 64; i += 2)
+			{
+				m_nUnshuffledHeader[i] = m_nPairs[i / 2].first;
+				m_nUnshuffledHeader[i + 1] = m_nPairs[i / 2].second;
+			}
+
+			FILE* m_pUnshuffledMetadataHeaderFile = nullptr;
+			fopen_s(&m_pUnshuffledMetadataHeaderFile, "DIA4A\\unshuffled-metadata-header.dat", "wb");
+			fwrite(m_nUnshuffledHeader, 1, sizeof(m_nUnshuffledHeader), m_pUnshuffledMetadataHeaderFile);
+			fclose(m_pUnshuffledMetadataHeaderFile);
+
+			std::vector<unsigned char> m_pGlobalMetadata = LoadFileBuffer(R"(RotMG Exalt_Data\il2cpp_data\Metadata\global-metadata.dat)");
+			if (!m_pGlobalMetadata.empty())
+			{
+				printf("[+] Found global-metadata.dat, Creating \"DIA4A\\global-metadata.dat\" With The Unshuffled Header\n");
+
+				memcpy(m_pGlobalMetadata.data(), m_nUnshuffledHeader, sizeof(m_nUnshuffledHeader));
+
+				FILE* m_pUnshuffledGlobalMetadataFile = nullptr;
+				fopen_s(&m_pUnshuffledGlobalMetadataFile, "DIA4A\\global-metadata.dat", "wb");
+				fwrite(m_pGlobalMetadata.data(), 1, m_pGlobalMetadata.size(), m_pUnshuffledGlobalMetadataFile);
+				fclose(m_pUnshuffledGlobalMetadataFile);
+			}
+		}
+		else
+		{
+			printf("[-] Failed To Unshuffle Offsets In The Header\n");
+		}
+	}
+	printf("[+] Finished Executing\n");
+	getchar();
+
+	return 0;
+}
